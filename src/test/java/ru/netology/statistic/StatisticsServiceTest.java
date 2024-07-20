bb811ceb42dd447d84abeae2ecc52fda1c0f79ce
nberez <nikol0880@outlook.com>
2024-07-20 21:12:17 +0300

    й

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    /*@Test
+    void findMax1(){
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {13, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }*/
+
+    @Test
+    void findMaxLast() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
